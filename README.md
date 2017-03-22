3f27523269dadf302bfed0f0d99ced1273d52bee
davide-rama <davide.ramaglietta@gmail.com>
2017-03-22 14:49:06 +0100

    Initial commit: working game with time traveling

---
+++ README.md
@@ -0,0 +1,9 @@
+### Tic Tac Toe 
+
+Intro to React. 
+
+`npm install` to install all dependencies
+
+`npm start` to start the application
+
+and then visit `http://localhost:8080/` to start to play!
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './components/Game';
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
